a3f874e59aa741b49e7cbebb9b72aefe012ec387
abakum <koka.abakum@gmail.com>
2024-03-09 13:17:17 +0300

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github/abakum/eex
+
+go 1.21.4
+
+require github.com/abakum/embed-encrypt v0.0.0-20240309090251-5db710414591 // indirect
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"io/fs"
+	"log"
+
+	"github.com/abakum/embed-encrypt/encryptedfs"
+)
+
+//go:generate go run internal/main.go
+
+//go:generate go run github.com/abakum/embed-encrypt
+
+//encrypted:embed hello.txt
+var hello string
+
+//encrypted:embed gopher.png
+var gopher []byte
+
+// encrypted:embed gopher.png
+//
+//encrypted:embed hello.txt "another.txt" "with spaces .txt"
+var multiplefiles encryptedfs.FS
+
+//encrypted:embed *.txt
+var glob encryptedfs.FS
+
+func main() {
+	c, err := fs.ReadFile(glob, "hello.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(hello, len(gopher), multiplefiles)
+
+	log.Println(string(c))
+}
